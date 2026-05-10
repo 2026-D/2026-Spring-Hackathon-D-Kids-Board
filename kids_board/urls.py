86b3afe6a23e8fca3b86c43169aa85fbd7ec34b2
minoru0220 <minoru0220@users.noreply.github.com>
2026-05-10 16:30:29 +0900

    fix: signup機能を再度修正

--- kids_board/urls.py
+++ kids_board/urls.py
@@ -2,5 +2,5 @@ from django.urls import path  # URLを書くためのpathを使う
 from .views import signup_view
 
 urlpatterns = [
-    path("signup/", signup_view, name="signup")  # http://localhost:8000/signup/
+    path("signup/", signup_view, name="signup"),  # http://localhost:8000/signup/
 ]
