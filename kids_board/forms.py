6d216b19b58994feb7b3c74a73e7d968c959f703
minoru0220 <minoru0220@users.noreply.github.com>
2026-05-11 22:39:19 +0900

    サインアップブランチの情報を取り込み

---
+++ kids_board/forms.py
@@ -0,0 +1,73 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):  # UserCreationFormを継承
+    email = forms.EmailField(  # メールアドレス入力欄を作る
+        required=True,  # 項目は必須入力
+        widget=forms.EmailInput(  # <input type="email">
+            attrs={  # HTMLの追加設定
+                "class": "form-control",  # Bootstrap用のCSSクラス
+                "placeholder": "メールアドレス",  # プレースホルダー
+            }
+        ),
+    )
+
+    class Meta:
+        # 標準Userを使う
+        model = User
+        # フォーム表示の入力項目を指定
+        fields = ("username", "email", "password1", "password2")
+        widgets = {
+            "username": forms.TextInput(  # 文字入力欄
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "ユーザー名",
+                }
+            ),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)  # 親クラス UserCreationForm の初期化処理
+
+        self.fields["username"].help_text = ""  # 説明文を空文字にする
+        self.fields["password1"].help_text = ""
+        self.fields["password2"].help_text = ""
+
+        self.fields["password1"].widget.attrs.update(  # <input> のclassやplaceholderを設定する
+            {
+                "class": "form-control",
+                "placeholder": "パスワード",
+            }
+        )
+
+        self.fields["password2"].widget.attrs.update(
+            {
+                "class": "form-control",
+                "placeholder": "パスワード確認",
+            }
+        )
+
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.label_suffix = ""  # フォームのlabelの最後につく文字を空にする
+
+        self.fields["username"].label = "ユーザー名"
+        self.fields["password"].label = "パスワード"
+
+        self.fields["username"].widget.attrs.update(
+            {
+                "class": "form-control",
+            }
+        )
+
+        self.fields["password"].widget.attrs.update(
+            {
+                "class": "form-control",
+            }
+        )
--- kids_board/views.py
+++ kids_board/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.contrib.auth import login  # 登録ユーザーをログイン状態にする
+from django.shortcuts import render, redirect  # renderはHTML表示 #redirectは別ページ移動
 
-# Create your views here.
+from .forms import SignUpForm  # forms.pyからSignUpFormを読み込む
+from .forms import LoginForm  # forms.pyからLoginFormを読み込む
+
+
+def signup_view(request):  # signup/へのアクセス時に動く処理
+    if request.method == "POST":  # 送信ボタンが押された時
+        form = SignUpForm(request.POST)  # 入力された内容をフォームに入れる
+
+        if form.is_valid():  # 入力チェック
+            user = form.save()  # ユーザーをUserテーブルに保存
+            login(request, user)  # 登録後、ログイン状態を維持
+            return redirect(
+                "signup"
+            )  # 登録が成功した後、homeを作成していない為、仮でsignupページへ移動させる
+    else:
+        form = SignUpForm()  # signup/ページを最初にを開いた時（GET）でフォームは空
+
+    return render(  # signup.htmlを画面表示する処理
+        request,  # ユーザーがブラウザから送ってきた情報
+        "kids_board/signup.html",  # templates/kids_board/signup.htmlを表示
+        {"form": form},  # Pythonで作ったformをHTMLに渡す
+    )
+
+
+def login_view(request):  # login/へのアクセス時に動く処理
+    if request.method == "POST":  # ログインボタンが押された時
+        form = LoginForm(request, data=request.POST)  # Django標準のログインフォーム
+
+        if form.is_valid():  # ログイン情報が正しいか確認
+            user = form.get_user()  # ログインが成功したユーザー
+            login(request, user)  # ユーザーをログイン状態にする
+            return redirect("home")  # ログイン成功後にhomeページへ移動
+    else:
+        form = LoginForm()  # 最初にページを開いた時
+
+    return render(  # login.html を表示する
+        request,
+        "kids_board/login.html",
+        {"form": form},
+    )
