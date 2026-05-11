0676803f69ec8ae13aa7456c4426e84d095a89cb
minoru0220 <61413589+minoru0220@users.noreply.github.com>
2026-05-11 23:25:36 +0900

    feat: signup機能を実装 (#13)
    * feat: signup機能を実装
    
    * fix: signup機能を修正
    
    * fix: signup機能を再度修正
    
    ---------
    
    Co-authored-by: minoru0220 <minoru0220@users.noreply.github.com>

---
+++ kids_board/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path  # URLを書くためのpathを使う
+from .views import signup_view
+
+urlpatterns = [
+    path("signup/", signup_view, name="signup"),  # http://localhost:8000/signup/
+]
