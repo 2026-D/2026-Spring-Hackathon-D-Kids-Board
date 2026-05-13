41efc873ee593fa74720e1acb87b26e259662d68
Kyoko-Sakamoto759 <sakamo.1992@gmail.com>
2026-05-13 14:11:07 +0900

    作成済みのhtmlファイルを追加、htmlファイルを表示するためのviews.pyとurl.py,settings.pyを記述,ruff formatによる修正 (#15)

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,32 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
+from kids_board.views import HomeView, KidsBoardView, PrepItemsMornView, PrepItemsView, ScheduleView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("kids_board.urls")),  # ""はhttp://localhost:8000/　#kids_board/urls.pyを使う
+    path(
+        "logout/", LogoutView.as_view(next_page="/home/"), name="logout"
+    ),  # todo:ログアウト後のリダイレクト先をtopに変更
+    path("home/", HomeView.as_view(template_name="kids_board/home.html"), name="home"),
+    path(
+        "kids_board/",
+        KidsBoardView.as_view(template_name="kids_board/kids_board.html"),
+        name="kids_board",
+    ),
+    path(
+        "prep_items_morn/",
+        PrepItemsMornView.as_view(template_name="kids_board/prep_items_morn.html"),
+        name="prep_items_morn",
+    ),
+    path(
+        "prep_items/",
+        PrepItemsView.as_view(template_name="kids_board/prep_items.html"),
+        name="prep_items",
+    ),
+    path(
+        "schedule/", ScheduleView.as_view(template_name="kids_board/schedule.html"), name="schedule"
+    )
 ]
