7bb278ab6225f31a7ef465161dd21ee89e9f6858
minoru0220 <61413589+minoru0220@users.noreply.github.com>
2026-05-14 23:25:33 +0900

    feat: ログイン機能の実装 (#17)
    * サインアップブランチの情報を取り込み
    
    * fix: signup登録後、loginページに遷移するように修正
    
    * feat: ログイン機能を実装
    
    ---------
    
    Co-authored-by: minoru0220 <minoru0220@users.noreply.github.com>

--- kids_board/views.py
+++ kids_board/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.contrib.auth import login  # 登録ユーザーをログイン状態にする
 from django.shortcuts import render, redirect  # renderはHTML表示 #redirectは別ページ移動
-from .forms import SignUpForm  # forms.pyからSignUpFormを読み込む
+from .forms import SignUpForm, LoginForm  # forms.pyからSignUpForm, LoginFormを読み込む
 from django.views.generic import TemplateView
 
 
@@ -10,11 +10,8 @@ def signup_view(request):  # signup/へのアクセス時に動く処理
         form = SignUpForm(request.POST)  # 入力された内容をフォームに入れる
 
         if form.is_valid():  # 入力チェック
-            user = form.save()  # ユーザーをUserテーブルに保存
-            login(request, user)  # 登録後、ログイン状態を維持
-            return redirect(
-                "signup"
-            )  # 登録が成功した後、homeを作成していない為、仮でsignupページへ移動させる
+            form.save()  # ユーザーをUserテーブルに保存
+            return redirect("login")  # 登録後、loginページへ移動
     else:
         form = SignUpForm()  # signup/ページを最初にを開いた時（GET）でフォームは空
 
@@ -22,6 +19,24 @@ def signup_view(request):  # signup/へのアクセス時に動く処理
         request,  # ユーザーがブラウザから送ってきた情報
         "kids_board/signup.html",  # templates/kids_board/signup.htmlを表示
         {"form": form},  # Pythonで作ったformをHTMLに渡す
+    )
+
+
+def login_view(request):  # login/へのアクセス時に動く処理
+    if request.method == "POST":  # ログインボタンが押された時
+        form = LoginForm(request, data=request.POST)  # Django標準のログインフォーム
+
+        if form.is_valid():  # ログイン情報が正しいか確認
+            user = form.get_user()  # ログインが成功したユーザー
+            login(request, user)  # ユーザーをログイン状態にする
+            return redirect("home")  # ログイン成功後にhomeページへ移動
+    else:
+        form = LoginForm()  # 最初にページを開いた時
+
+    return render(  # login.html を表示する
+        request,
+        "kids_board/login.html",
+        {"form": form},
     )
 
 
@@ -57,4 +72,3 @@ class PrepItemsView(TemplateView):
 # todo:LoginRequiredMixinを追加
 class ScheduleView(TemplateView):
     template_name = "kids_board/schedule.html"
-
