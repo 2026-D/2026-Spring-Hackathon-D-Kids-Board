ef3d62fca0353803f3ec2118a27615ce3647b934
minoru0220 <minoru0220@users.noreply.github.com>
2026-05-16 17:28:14 +0900

    feat: ログアウト機能を実装

--- kids_board/views.py
+++ kids_board/views.py
@@ -1,5 +1,8 @@
 # Create your views here.
-from django.contrib.auth import login  # 登録ユーザーをログイン状態にする
+from django.contrib.auth import (
+    login,
+    logout,
+)  # 登録ユーザーをログイン状態にする # ログアウト状態にする
 from django.shortcuts import render, redirect  # renderはHTML表示 #redirectは別ページ移動
 from .forms import SignUpForm, LoginForm  # forms.pyからSignUpForm, LoginFormを読み込む
 from django.views.generic import TemplateView
@@ -38,6 +41,11 @@ def login_view(request):  # login/へのアクセス時に動く処理
         "kids_board/login.html",
         {"form": form},
     )
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
 
 
 # todo:LoginRequiredMixinを追加
