f5ed9c8b1cbc41cc97771de7ba3708dae535fdbb
Kyoko-Sakamoto759 <sakamo.1992@gmail.com>
2026-05-19 22:40:57 +0900

    home.htmlの表示について、views.pyとフロントのテンプレートの分岐の整理。

--- kids_board/views.py
+++ kids_board/views.py
@@ -5,9 +5,9 @@ from django.contrib.auth import (
 )  # 登録ユーザーをログイン状態にする # ログアウト状態にする
 from django.shortcuts import render, redirect  # renderはHTML表示 #redirectは別ページ移動
 from .forms import SignUpForm, LoginForm  # forms.pyからSignUpForm, LoginFormを読み込む
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from django.urls import reverse_lazy
-from .models import PrepItem
+from .models import PrepItem, Child
 from django.contrib.auth.mixins import LoginRequiredMixin  # ログイン必須のクラスを読み込む
 
 
@@ -51,8 +51,16 @@ def logout_view(request):  # logout/へのアクセス時に動く処理
     return redirect("login")  # ログアウト後にloginページへ移動する
 
 
-class HomeView(LoginRequiredMixin, TemplateView):
-    template_name = "/home.html"
+class HomeView(LoginRequiredMixin, ListView):
+    model = Child
+    template_name = "kids_board/home.html"
+    context_object_name = "children"
+    ordering = ["created_at"]  # 子供の表示順を作成日時順にするための指定
+    print("children:", context_object_name)
+
+    def get_queryset(self):
+        # ログインしているユーザー（ファミリー）の子供だけを表示するためのクエリセットを返す
+        return Child.objects.filter(parent=self.request.user, deleted_at__isnull=True)
 
 
 class KidsBoardView(LoginRequiredMixin, TemplateView):
