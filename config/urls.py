718604a53f943cef2d3c21d129cf9781c2d213f1
minoru0220 <minoru0220@users.noreply.github.com>
2026-05-25 21:25:53 +0900

    fix: commitが残っていたため再度push

--- config/urls.py
+++ config/urls.py
@@ -17,56 +17,20 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from kids_board.views import (
-    HomeView,
-    KidsBoardView,
-    PrepItemsMornView,
-    PrepItemsView,
-    PrepItemEditView,
-    CustomItemsEditView,
-    NewItemsEditView,
-    ScheduleView,
-    SettingsView,
-)
+# from kids_board.views import (
+#     HomeView,
+#     KidsBoardView,
+#     PrepItemsMornView,
+#     PrepItemsView,
+#     PrepItemEditView,
+#     CustomItemsEditView,
+#     NewItemsEditView,
+#     ScheduleView,
+#     SettingsView,
+# )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("kids_board.urls")),  # ""はhttp://localhost:8000/　#kids_board/urls.pyを使う
-    path("home/", HomeView.as_view(template_name="kids_board/home.html"), name="home"),
-    path(
-        "kids_board/",
-        KidsBoardView.as_view(template_name="kids_board/kids_board.html"),
-        name="kids_board",
-    ),
-    path(
-        "prep_items_morn/",
-        PrepItemsMornView.as_view(template_name="kids_board/prep_items_morn.html"),
-        name="prep_items_morn",
-    ),
-    path(
-        "prep_items/",
-        PrepItemsView.as_view(template_name="kids_board/prep_items.html"),
-        name="prep_items",
-    ),
-    path(
-        "prep_items/<int:prep_item_id>/edit/",
-        PrepItemEditView.as_view(template_name="kids_board/prep_items.html"),
-        name="prep_items_edit_api",
-    ),
-    path(
-        "custom_items/",
-        CustomItemsEditView.as_view(template_name="kids_board/custom_items.html"),
-        name="custom_items",
-    ),
-    path(
-        "custom_items/new/",
-        NewItemsEditView.as_view(template_name="kids_board/new_items.html"),
-        name="new_items",
-    ),
-    path(
-        "schedule/", ScheduleView.as_view(template_name="kids_board/schedule.html"), name="schedule"
-    ),
-    path(
-        "settings/", SettingsView.as_view(template_name="kids_board/settings.html"), name="settings"
-    ),
+    # path("home/", HomeView.as_view(template_name="kids_board/home.html"), name="home"),
 ]
