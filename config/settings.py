010281cac9d7021148508dcf29f76f554de0743b
Shiho-F <chibimaro0325@icloud.com>
2026-05-26 23:13:59 +0900

    fix:Docker環境変数設定とhealthcheckを追加

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         "NAME": os.environ.get("DATABASE_NAME"),
         "USER": os.environ.get("DATABASE_USER"),
         "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
-        "HOST": os.environ.get("DATABASE_HOST"),
+        "HOST": os.environ.get("DATABASE_HOST", "db"),
         "PORT": 3306,
     }
 }
